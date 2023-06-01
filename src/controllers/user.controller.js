02a62f458f8023f96904194963840821e79f0aac
matheusbiscalchim <matheusbiscalchimmm@gmail.com>
2023-06-01 17:29:08 -0300

    criacao post usuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,22 @@
-const create = (req,res)  => {
+const userSerivce = require('../services/user.serivce');
+
+const create = async(req,res)  => {
   const {nome,email,senha} = req.body;
 
   if (!nome || !email || !senha ) {
     res.status(400).send({mensagem:"Envie todos os campos para registrar"})
   }
 
+  const user = await userSerivce.create(req.body);
+
+  if (!user) {
+    return res.status(400).send({ message: "Erro na criação de usuário" });
+  }
+
   res.status(201).send({
     mensagem:"Usuario criado com sucesso",
     usuario:{
+      id:user._id,
       nome,
       email
     }
---
+++ src/database/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+  console.log("Espere conectando ao banco de dados")
+
+  mongoose.connect("mongodb+srv://root:root@curitibakicluster.suf46h9.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
+  ).then(() => console.log("MongoDB Atlas Conectado")).catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
